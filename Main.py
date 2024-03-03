d77343ffd3c80ff8c9535c2bd65ab0ba9eb62a1f
luongvv <lvvuong@student.rtc.edu>
2024-03-03 15:25:54 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,19 @@
+# CNE 340 - Network Databases and Structured Query Language (SQL)
+# Winter 2024
+# Group project name: USA Crime Rate 2024
+# Project members: Tyler Sabin, Van Vuong
+
+# Import section
 import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from sqlalchemy import create_engine
+
+# SQL connection and Database
+hostname = '172.0.0.1'
+uname = 'root'
+pwd = ''
+dbname = 'USA_Crime_Rate_2024'
+
+connection_string = f'mysql+pymysql://{uname}:{pwd}@{hostname}/{dbname}'
+engine = create_engine(connection_string)
